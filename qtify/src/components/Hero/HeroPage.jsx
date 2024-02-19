f14708915181661110086cb7d6e28081e648a8b6
Ataur Rehman <rehmangour04@gmail.com>
2024-02-19 11:33:51 +0530

    add hero section

--- qtify/src/components/Hero/HeroPage.jsx
+++ qtify/src/components/Hero/HeroPage.jsx
@@ -1,14 +1,18 @@
 /** @format */
-import "./heropage.css";
-import HeroImage from "../../assets/hero.png";
+import style from "./HeroSection.module.css";
+import HeroImage from "../../assets/vibrating-headphone 1.png";
 const HeroPage = () => {
   return (
     <>
-      <div className="container-image">
-        <div className="hero-img">
-          <img src={HeroImage} alt="hero" />
+      <section className={style.heroSection}>
+        <div className={style.heroImgContainer}>
+          <div className={style.heroText}>
+            <p>100 Thousand Songs, ad-free</p>
+            <p>Over thousands podcast episodes</p>
+          </div>
+          <img src={HeroImage} alt="headphone" className={style.headphoneImg} />
         </div>
-      </div>
+      </section>
     </>
   );
 };
